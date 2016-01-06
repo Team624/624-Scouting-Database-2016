dfb4cdf2ab20e600170eca041e3a9d0b330cbe0c
valeria <h0503687@students.katyisd.org>
2016-01-05 18:24:02 -0600

    This styles the login page, login error message, and logout message.

--- HeadTemplate.php
+++ HeadTemplate.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="css/indexstyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=0.5">
     <title>624 Scouting</title>
 	
