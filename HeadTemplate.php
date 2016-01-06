9ebae92b92ad413f73e20f1f50b10ef799945ca6
Justin Kleiber <jkleiber8@gmail.com>
2016-01-05 21:04:38 -0600

    fixes style problems

--- HeadTemplate.php
+++ HeadTemplate.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
-    <link rel="stylesheet" type="text/css" href="css/indexstyle.css">
+    
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=0.5">
 		<title>624 Scouting</title>
 	
 		<link rel="icon" type="image/png" href="favicon.ico">
+		
+		<!-- Other Styles -->
 		<link rel="stylesheet" type="text/css" href="css/responsive_navbar.css">
+		
+		<!-- Specific Page Styles -->
+		<link rel="stylesheet" type="text/css" href="css/indexstyle.css">
 	
 	</head>
 
