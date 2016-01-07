d47efcb1b9d4c6eb1bb75f0818e6fc02993989b3
j0$ep# n0rm@n <n0807701j2d2@gmail.com>
2016-01-06 21:08:50 -0600

    adds a few things to the dataform

--- DataEntry.php
+++ DataEntry.php
@@ -4,4 +4,21 @@ include("HeadTemplate.php");
 include("UserVerification.php");
 include("navbar.php");
 ?>
-
+<head>	<link rel="stylesheet" type="text/css" href="css/mainpagestyle.css"> 
+		<link rel="stylesheet" type="text/css" href="css/dataform.css">
+</head>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="page_container">
+	<form class="datafield">
+		Name:<br>
+		<input type = "text" name = "username">
+		<br>
+		Team #:<br>
+		<input type="text" name="teamName">
+		<br>
+	</form>
+</div>
