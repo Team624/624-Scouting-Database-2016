78a4c33c15e0eb1a9d259988ce67bb530701e5db
Justin Kleiber <jkleiber8@gmail.com>
2016-01-07 14:28:12 -0600

    Adds more tings to the data form. TODO: Add database functionality for inserting data.

--- DataEntry.php
+++ DataEntry.php
@@ -17,7 +17,9 @@ include("navbar.php");
 </div>
 <div class="page_container">
 	<div class="form_container">
-		<form class="datafield">
+		<form class="datafield" method="post">
+		
+			<h2>Basic Data</h2>
 			<table class="red">
 				<tr>
 					<td>Match #</td>
@@ -30,13 +32,59 @@ include("navbar.php");
 					<td><input type="number" name="scoutID" class="small_num"></td>
 				</tr>
 			</table>
+			
 			<br>
+			
+			<h2>Autonomous</h2>
+			<table class="blue">
+				<td>Autonomous stuff will go here one day!!!</td>
+			</table>
+			
+			<br>
+			
+			<h2>Teleop</h2>
+			<table class="green">
+				<td>Teleop stuff will go here one day!!!</td>
+			</table>
+			
+			<h2>Robot Issues</h2>
+			<table class="red">
+				<tr>
+					<td>No Show</td>
+					<td><input type="checkbox" name="no_show"></input></td>
+				</tr>
+					<td>Tipped Over</td>
+					<td><input type="checkbox" name="tipped"></input></td>
+				</tr>
+					<td>Lost Comms</td>
+					<td><input type="checkbox" name="lost_comm"></input></td>
+				</tr>
+					<td>Mechanical Failure</td>
+					<td><input type="checkbox" name="mech_fail"></input></td>
+				</tr>
+			</table>
+			
+			<br>
+			
+			<h2>Driver Data</h2>
+			<table class="green">
+				<tr>
+					<td>Driving/Maneuverability</td>
+				</tr>
+				<tr>
+					<td><input type="number" name="drive_man" class="small_num"></input></td>
+				</tr>
+			</table>
+			
+			<br>
+			
+			<h2>Comments</h2>
 			<table class="blue">
 				<tr>
 					<td>Notes:</td>
 				</tr>
 				<tr>
-					<td><textarea rows=5 cols=20></textarea></td>
+					<td><textarea rows=5 cols=30 name="notes"></textarea></td>
 				</tr>
 			</table>
 			<br>
