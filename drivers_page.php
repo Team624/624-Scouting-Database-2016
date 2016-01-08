48f5071f936c5c81695830578abbe163a6082838
n0807701j2d2 <n0807701j2d2@gmail.com>
2016-01-07 20:52:46 -0600

    Driver Page, continuing...

--- drivers_page.php
+++ drivers_page.php
@@ -11,28 +11,20 @@ if(isset($valid_user) && isset($user_type))
 
 <!-- Make this page Tablet Friendly -->
 <head>
-<link rel="stylesheet" type="text/css" href="css/DankDriverPageStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/DankDriverPageStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/mainpagestyle.css"> 
+	<link rel="stylesheet" type="text/css" href="css/dataform.css">
 </head>
 
+<div class="page_container">
+	<div class="form_container">
+		<form class="datafield" method="post">
 <div class="container">
-<br>
-<br>
-<br>
 <br>
 <br>
 	<h2><span>Drive Team Page</span></h2>
 	
-	<div>
-	Put Zoomed in Ranking Here <!-- 5 teams above and below in rankings, if 0 above, put only 5 below -->
-	</div>
 	
-	<div>
-	Put Performance Charts Here
-	</div>
-	
-	<div>
-	Put Upcoming Matches Here
-	</div>
 </div>
 
 <?php
