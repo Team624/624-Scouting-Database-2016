6fbcc83414ac95bb9a9312a0b05bab6838e229a4
valeriah <h0503687@students.katyisd.org>
2016-01-07 21:35:15 -0600

    Attempt at getting the API to work

--- Rankings.php
+++ Rankings.php
@@ -3,6 +3,16 @@
 include("HeadTemplate.php");
 include("UserVerification.php");
 include("navbar.php");
+echo "<br><br><br><br><br><br><br>";
+include("read_ini.php");
+
+include("api_connect.php");
+
+
+	$url = "https://frc-api.usfirst.org/api/v1.0/rankings/2016/TXHO";
+	$response=file_get_contents($url,false,$context);
+	
+var_dump($response);
 ?>
 <head>	<link rel="stylesheet" type="text/css" href="css/RankingsStyle.css"> </head>
 <br>
