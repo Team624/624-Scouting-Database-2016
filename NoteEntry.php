6f22c7b5b328e5bc987c030b8c903036bba3cead
Anurag-Ghosh <onotheeawesome@gmail.com>
2016-01-12 21:48:50 -0600

    Worked on html elements of Note Entry page

--- NoteEntry.php
+++ NoteEntry.php
@@ -5,3 +5,29 @@ include("UserVerification.php");
 include("kick_intruders.php");
 include("navbar.php");
 ?>
+<head>	
+	<link rel="stylesheet" type="text/css" href="css/mainpagestyle.css">
+	<link rel="stylesheet" type="text/css" href="css/NoteEntryStyle.css"> 
+</head>
+<br>
+<br>
+<br>
+<br>
+<div class="title">
+	<h1>Note Entry</h1>
+</div>
+<div class="page_container">
+<form class="NoteEntry" method="post">
+<p>Search For Da Team</p>
+<input type="number" name="selectteam">
+<br>
+<br>
+<br>
+<textarea rows="5" cols="30">
+Type notes in here!
+</textarea>
+<br>
+<br>
+<input type="submit" value="Enter!">
+</form>
+</div>
