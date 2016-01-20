f6c985a8f03964be6c4442a67d12e29f2705c591
Anurag-Ghosh <onotheeawesome@gmail.com>
2016-01-19 21:25:02 -0600

    Sets up HTML and PHP for  Team and Match search

--- NoteEntry.php
+++ NoteEntry.php
@@ -36,7 +36,6 @@ if(isset($_POST['submitnotes'])){
 if(!empty($_POST['selectteam']) && !empty($_POST['notes'])) {
 	$teamselect=$_POST['selectteam'];
 	$notes=$_POST['notes'];	
-	// commenting this out for now, never know when It will have to use it later. $con=mysqli_connect('localhost','username','password','databasename','portnumba') or die(mysqli_error());
 	include("db_connect.php");
 	$query = "INSERT INTO note_entry (selectteam,notes) VALUES ('$teamselect','$notes')";
 	$result = $mysqli->query($query);
--- Search.php
+++ Search.php
@@ -4,12 +4,58 @@ include("HeadTemplate.php");
 include("UserVerification.php");
 include("kick_intruders.php");
 include("navbar.php");
+include("db_connect.php");
 ?>
-<head>	<link rel="stylesheet" type="text/css" href="css/SearchStyle.css"> </head>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/NoteEntryStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/mainpagestyle.css">
+	<link rel="stylesheet" type="text/css" href="css/SearchStyle.css"> 
+</head>
 <br>
 <br>
 <br>
 <br>
-<div class = "title" >
+<div class = "title">
 	<h1> Search for Teams and Matches </h1>
 </div>
+<div class="page_container">
+<form class="Searchforsearch" method="post">
+<br>
+<span class="teamsearch">Search Fo:</span><select>
+  <option value="teams" name="choseteam">Teams</option>
+  <option value="matches" name="chosematch">Matches</option>
+ </select>&nbsp;&nbsp;&nbsp;<input type="number" name="number">
+<br>
+<br>
+<br>
+<input type="submit" value="Enter!" class="subButton" name="searchsubmit">
+</form>
+</div>
+<?php
+if(isset($_POST['searchsubmit'])){
+if(!empty($_POST['choseteam']) && !empty($_POST['number'])) {
+	$teamnumber=$_POST['number'];
+	$query = //"INSERT INTO note_entry (selectteam,notes) VALUES ('$teamselect','$notes')";
+	$result = $mysqli->query($query);
+	if($result) {
+	echo"Successfully blazed it";	
+	}
+	else {
+	echo"NOPE!";	
+	}
+}elseif(!empty($_POST['chosematch']) && !empty($_POST['number'])) {
+	$matchnumber=$_POST['number'];
+	$query = //"INSERT INTO note_entry (selectteam,notes) VALUES ('$teamselect','$notes')";
+	$result = $mysqli->query($query);
+	if($result) {
+	echo"Successfully blazed it";	
+	}
+	else {
+	echo"NOPE!";	
+	}
+	
+} else{
+	echo"Nothin";
+}
+}
+?>
