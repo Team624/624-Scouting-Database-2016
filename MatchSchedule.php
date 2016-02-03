9134af81d331dc2d9fd241cae394f6ec985ab80a
Viraj Joshi <viraj.j1@gmail.com>
2016-02-02 21:07:25 -0600

    Added MatchSchedule.php functionality

--- MatchSchedule.php
+++ MatchSchedule.php
@@ -23,9 +23,9 @@ $response1 = file_get_contents($url1,false,$context);
 </div>
 <table class = "rankingsTable" >
 		<thead>
-			<th><span>TeamNumber</span></th>
 			<th>startTime</th>
 			<th>matchNumber</th>
+			<th><span>TeamNumber</span></th>
 			<th>station</th>
 		</thead>
 	
@@ -34,35 +34,38 @@ $response1 = file_get_contents($url1,false,$context);
 <?php
 
 mysqli_select_db($mysqli,"mynewdatabase3");
-$json1 = json_decode($response1, true);
-//echo json_encode($json1/*, JSON_PRETTY_PRINT*/);
+$json1 = json_decode($response1,true);
+//echo json_encode($json1, JSON_PRETTY_PRINT);
 
 foreach($json1 as $match){
-	
 	foreach($match as $move){
-		foreach($match as $move){
-		$teamNumber1=$move["teamNumber"];
 		$startTime=$move["startTime"];
 		$matchNumber=$move["matchNumber"];
-		$station=$move["station"];
 		
-	
-		$sql1="INSERT INTO matchschedule(TeamNumber,startTime,matchNumber,station)
-		VALUES('$teamNumber1','$startTime','$matchNumber','$station')";
-		
+		$sql1="INSERT INTO matchschedule(startTime,matchNumber,teamNumber,station)
+		VALUES('$startTime','$matchNumber','$teamNumber1','$station')";
+		foreach($move[Teams] as $t){
+			$teamNumber1=$t['teamNumber'];
+			$station=$t['station'];
 		mysqli_query($mysqli,$sql1);
 		?>
 		<tr>
-						<td><?php echo $teamNumber1; ?></td>
+						
 						<td><?php echo $startTime; ?></td> 
 						<td><?php echo $matchNumber; ?></td> 
+						<td><?php echo $teamNumber1; ?></td>
 						<td><?php echo $station; ?></td> 
+						
 		</tr>
 		<?php	
 		
+			
+		}
 	}
 }
 ?>
+					
+					</tr>
 </tbody>
 	</table>
 
