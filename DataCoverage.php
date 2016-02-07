526b0a356700416e6a15ad3e5785023b0d2ffa1d
Lenovo 2.0 <Willalasater@gmail.com>
2016-02-06 20:47:46 -0600

    started changes to DataCoverage

--- DataCoverage.php
+++ DataCoverage.php
@@ -4,6 +4,7 @@ include("HeadTemplate.php");
 include("UserVerification.php");
 include("kick_intruders.php");
 include("navbar.php");
+include("db_connect.php");
 ?>
 <head>	<link rel="stylesheet" type="text/css" href="css/DataCoverageStyle.css"> </head>
 <br>
@@ -16,13 +17,17 @@ include("navbar.php");
 <div class="page-container">
 	<table class="Schedule-table">
 		<tr>
-			<th>Match</th>
-			<th>Red 1</th>
-			<th>Red 2</th>
-			<th>Red 3</th>
-			<th>Blue 1</th>
-			<th>Blue 2</th>
-			<th>Blue 3</th>
+			<th class="table-top">Match</th>
+			<th class="table-top">Red 1</th>
+			<th class="table-top">Red 2</th>
+			<th class="table-top">Red 3</th>
+			<th class="table-top">Blue 1</th>
+			<th class="table-top">Blue 2</th>
+			<th class="table-top">Blue 3</th>
+		</tr>
+			
+		<tr>
+
 		</tr>
 	</table>
 </div>
