b75403aaf695bc441bc63c968dd45155991c6bdc
Lenovo 2.0 <Willalasater@gmail.com>
2016-02-09 21:11:35 -0600

    Made the table work, woop

--- DataCoverage.php
+++ DataCoverage.php
@@ -4,7 +4,12 @@ include("HeadTemplate.php");
 include("UserVerification.php");
 include("kick_intruders.php");
 include("navbar.php");
+include("api_connect.php");
+include("read_ini.php");
 include("db_connect.php");
+$url1="https://frc-api.firstinspires.org/v2.0/2015/schedule/TXHO?tournamentLevel=qual";
+
+$response1 = file_get_contents($url1,false,$context);
 ?>
 <head>	<link rel="stylesheet" type="text/css" href="css/DataCoverageStyle.css"> </head>
 <br>
@@ -16,18 +21,41 @@ include("db_connect.php");
 </div>
 <div class="page-container">
 	<table class="Schedule-table">
-		<tr>
-			<th class="table-top">Match</th>
-			<th class="table-top">Red 1</th>
-			<th class="table-top">Red 2</th>
-			<th class="table-top">Red 3</th>
-			<th class="table-top">Blue 1</th>
-			<th class="table-top">Blue 2</th>
-			<th class="table-top">Blue 3</th>
+		<tr class="top-bar">
+			<th class="table-top"><b>Match</b></th>
+			<th class="table-top"><b>Red 1</b></th>
+			<th class="table-top"><b>Red 2</b></th>
+			<th class="table-top"><b>Red 3</b></th>
+			<th class="table-top"><b>Blue 1</b></th>
+			<th class="table-top"><b>Blue 2</b></th>
+			<th class="table-top"><b>Blue 3</b></th>
 		</tr>
-			
-		<tr>
+<?php
+$json1 = json_decode($response1,true);
 
+	foreach($json1 as $match){
+		foreach($match as $move){
+		$matchNumber=$move["matchNumber"];
+		
+		
+?>	
+		<tr>
+			<td class="side-bar"><b><?=$matchNumber; ?></b></td>
+			<?php
+				foreach($move[Teams] as $t){
+					$teamNumber1=$t['teamNumber'];
+			?>
+			<td><?=$teamNumber1?></td>
+			<?php
+				}
+			?>
 		</tr>
+<?php	
+		
+		}
+
+	}
+			
+?>
 	</table>
 </div>
