b72c4612609ed2495d80a225d3bef175de43cbe7
Anurag-Ghosh <onotheeawesome@gmail.com>
2016-02-16 16:35:02 -0600

    Makes some changes to Match Schedule and Data Entry, STILL IN PROGRESS

--- DataEntry.php
+++ DataEntry.php
@@ -41,11 +41,11 @@ include("navbar.php");
 					<td>Defense 5</td>
 				</tr>
 				<tr>
-					<td><input type="text" name="def_type_1" class="small_num"></td>
-					<td><input type="text" name="def_type_2" class="small_num"></td>
-					<td><input type="text" name="def_type_3" class="small_num"></td>
-					<td><input type="text" name="def_type_4" class="small_num"></td>
-					<td><input type="text" name="def_type_5" class="small_num"></td>
+					<td><input type="number" name="def_type_1" class="small_num"></td>
+					<td><input type="number" name="def_type_2" class="small_num"></td>
+					<td><input type="number" name="def_type_3" class="small_num"></td>
+					<td><input type="number" name="def_type_4" class="small_num"></td>
+					<td><input type="number" name="def_type_5" class="small_num"></td>
 				</tr>
 			</table>
 			
@@ -137,7 +137,40 @@ include("navbar.php");
 				</tr>
 			</table>
 			<br>
-			<input type="submit" class="subButton"></input>
+			<input type="submit" class="subButton" name="dataSubmit"></input>
 		</form>
 	</div>
+	<?php
+	if(isset($_POST['dataSubmit'])){
+	$matchNum=$_POST['match_num'];
+	$teamNum=$_POST['team_num'];
+	$scoutID=$_POST['scoutID'];
+	$def_type_1=$_POST['def_type_1'];
+	$def_type_2=$_POST['def_type_2'];
+	$def_type_3=$_POST['def_type_3'];
+	$def_type_4=$_POST['def_type_4'];
+	$def_type_5=$_POST['def_type_5'];
+	$def_1=$_POST['def_1'];
+	$def_2=$_POST['def_2'];
+	$def_3=$_POST['def_3'];
+	$def_4=$_POST['def_4'];
+	$def_5=$_POST['def_5'];
+	$ball_shot=$_POST['ball_shot'];
+	$balls_scored=$_POST['balls_scored'];
+	$no_show=$_POST['no_show'];
+	$tipped=$_POST['tipped'];
+	$lost_comm=$_POST['lost_comm'];
+	$mech_fail=$_POST['mech_fail'];
+	$drive_man=$_POST['drive_man'];
+	$notes=$_POST['notes'];
+	//$fields = array($matchNum,$teamNum,$def_type_1,$def_type_2,$def_type_3,$def_type_4,$def_type_5,$drive_man,$notes);
+	//foreach($fields as $fieldname){
+	//if(!empty($fieldname)){
+	$query = "INSERT INTO match_data (match_number,team_number,scout_id,def_type_1,def_type_2,def_type_3,def_type_4,def_type_5,def_1,def_2,def_3,def_4,def_5,ball_shot,balls_scored,no_show,tipped,lost_comm,mech_fail,drive_rating,notes) VALUES ('$matchNum','$teamNum','$scoutID','$def_type_1','$def_type_2','$def_type_3','$def_type_4','$def_type_5','$def_1','$def_2','$def_3','$def_4','$def_5','$ball_shot','$balls_scored','$no_show','$tipped','$lost_comm','$mech_fail','$drive_man','$notes')";
+	$result = $mysqli->query($query);
+	
+	//}	
+	//}	
+	}
+	?>
 </div>
