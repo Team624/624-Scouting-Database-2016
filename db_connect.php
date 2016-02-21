0554b832964be86f5ba3cd533b90716a1c30f99a
Lenovo 2.0 <Willalasater@gmail.com>
2016-02-20 22:41:16 -0600

    Did more on raw data, its not done yet, rip

--- db_connect.php
+++ db_connect.php
@@ -12,11 +12,11 @@
 	
 
 	
-/*this is if you want to connect to the team624 database
+//this is if you want to connect to the team624 database
 	//$dbhost = $ini['dbhost'];
 	//$dbname = $ini['dbname'];
 	//$dbuser = $ini['dbuser'];
-	$dbpass = $ini['dbpass'];*/
+	//$dbpass = $ini['dbpass'];
 	
 	$mysqli = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 	
