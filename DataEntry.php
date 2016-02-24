8e8981cc2363fd768fbf1891cc3cf3feb24431dd
Lenovo 2.0 <Willalasater@gmail.com>
2016-02-23 20:15:01 -0600

    add ints to the postes, fixed my strugles

--- DataEntry.php
+++ DataEntry.php
@@ -351,87 +351,86 @@ include("db_connect.php");
 	<?php
 	if(isset($_POST['dataSubmit'])){
 	//Basic Data	
-	$matchNum=$_POST['match_num'];
-	$teamNum=$_POST['team_num'];
-	$scoutID=$_POST['scoutID'];
+	$matchNum=(int)$_POST['match_num'];
+	$teamNum=(int)$_POST['team_num'];
+	$scoutID=(int)$_POST['scoutID'];
 	
-	
-	$def_category_2=$_POST['def_category_2'];
-	$def_category_3=$_POST['def_category_3'];
-	$def_category_4=$_POST['def_category_4'];
-	$def_category_5=$_POST['def_category_5'];
+	$def_category_2=(int)$_POST['def_category_2'];
+	$def_category_3=(int)$_POST['def_category_3'];
+	$def_category_4=(int)$_POST['def_category_4'];
+	$def_category_5=(int)$_POST['def_category_5'];
 	//Auton
-	$auto_High_Scored=$_POST['auto_High_Scored'];
-	$auto_Low_Scored=$_POST['auto_Low_Scored'];
+	$auto_High_Scored=(int)$_POST['auto_High_Scored'];
+	$auto_Low_Scored=(int)$_POST['auto_Low_Scored'];
 	
-	$auto_High_Miss=$_POST['auto_High_Miss'];
-	$auto_Low_Miss=$_POST['auto_Low_Miss'];
+	$auto_High_Miss=(int)$_POST['auto_High_Miss'];
+	$auto_Low_Miss=(int)$_POST['auto_Low_Miss'];
 	
 	
-	$auto_Defenses_Reached_Sucess=$_POST['auto_Defenses_Reached_Sucess'];
-	$auto_Defenses_Crossed_Sucess=$_POST['auto_Defenses_Crossed_Sucess'];
+	$auto_Defenses_Reached_Sucess=(int)$_POST['auto_Defenses_Reached_Sucess'];
+	$auto_Defenses_Crossed_Sucess=(int)$_POST['auto_Defenses_Crossed_Sucess'];
 	
-	$auto_Defenses_Reached_Failed=$_POST['auto_Defenses_Reached_Failed'];
-	$auto_Defenses_Crossed_Failed=$_POST['auto_Defenses_Crossed_Failed'];
+	$auto_Defenses_Reached_Failed=(int)$_POST['auto_Defenses_Reached_Failed'];
+	$auto_Defenses_Crossed_Failed=(int)$_POST['auto_Defenses_Crossed_Failed'];
 	
-	$auto_Start_Location=$_POST['auto_Start_Location'];
+	$auto_Start_Location=(int)$_POST['auto_Start_Location'];
 
-	if($_POST['Auto_Boulder_Grab']=="on"){$Auto_Boulder_Grab=1;}
-	if($_POST['Auto_StartWithBoulder']=="on"){$Auto_StartWithBoulder=1;}
+	if((int)$_POST['Auto_Boulder_Grab']=="on"){$Auto_Boulder_Grab=1;}
+	if((int)$_POST['Auto_StartWithBoulder']=="on"){$Auto_StartWithBoulder=1;}
 	//Teleop
-	$def_crossed_1=$_POST['def_1_crossed'];
-	$def_crossed_2=$_POST['def_2_crossed'];
-	$def_crossed_3=$_POST['def_3_crossed'];
-	$def_crossed_4=$_POST['def_4_crossed'];
-	$def_crossed_5=$_POST['def_5_crossed'];
+	$def_crossed_1=(int)$_POST['def_1_crossed'];
+	$def_crossed_2=(int)$_POST['def_2_crossed'];
+	$def_crossed_3=(int)$_POST['def_3_crossed'];
+	$def_crossed_4=(int)$_POST['def_4_crossed'];
+	$def_crossed_5=(int)$_POST['def_5_crossed'];
 	
-	$def_1_weakened=$_POST['def_1_weakened'];
-	$def_2_weakened=$_POST['def_2_weakened'];
-	$def_3_weakened=$_POST['def_3_weakened'];
-	$def_4_weakened=$_POST['def_4_weakened'];
-	$def_5_weakened=$_POST['def_5_weakened'];
+	$def_1_weakened=(int)$_POST['def_1_weakened'];
+	$def_2_weakened=(int)$_POST['def_2_weakened'];
+	$def_3_weakened=(int)$_POST['def_3_weakened'];
+	$def_4_weakened=(int)$_POST['def_4_weakened'];
+	$def_5_weakened=(int)$_POST['def_5_weakened'];
 
-	$def_1_speed=$_POST['def_1_speed'];
-	$def_2_speed=$_POST['def_2_speed'];
-	$def_3_speed=$_POST['def_3_speed'];
-	$def_4_speed=$_POST['def_4_speed'];
-	$def_5_speed=$_POST['def_5_speed'];
+	$def_1_speed=(int)$_POST['def_1_speed'];
+	$def_2_speed=(int)$_POST['def_2_speed'];
+	$def_3_speed=(int)$_POST['def_3_speed'];
+	$def_4_speed=(int)$_POST['def_4_speed'];
+	$def_5_speed=(int)$_POST['def_5_speed'];
 	
-	if($_POST['def_1_ball']== "on"){$def_1_ball=1;}
-	if($_POST['def_2_ball']== "on"){$def_2_ball=1;}
-	if($_POST['def_3_ball']== "on"){$def_3_ball=1;}
-	if($_POST['def_4_ball']== "on"){$def_4_ball=1;}
-	if($_POST['def_5_ball']== "on"){$def_5_ball=1;}
+	if((int)$_POST['def_1_ball']== "on"){$def_1_ball=1;}
+	if((int)$_POST['def_2_ball']== "on"){$def_2_ball=1;}
+	if((int)$_POST['def_3_ball']== "on"){$def_3_ball=1;}
+	if((int)$_POST['def_4_ball']== "on"){$def_4_ball=1;}
+	if((int)$_POST['def_5_ball']== "on"){$def_5_ball=1;}
 	//Shooting variables
-	$batter_high_Scored=$_POST['batter_high_Scored'];
-	$batter_low_Scored=$_POST['batter_low_Scored'];
-	$batter_high_Miss=$_POST['batter_high_Miss'];
-	$batter_low_Miss=$_POST['batter_low_Miss'];
-	$courtyard_high_Scored=$_POST['courtyard_high_Scored'];
-	$courtyard_low_Scored=$_POST['courtyard_low_Scored'];
-	$courtyard_high_Miss=$_POST['courtyard_high_Miss'];
-	$courtyard_low_Miss=$_POST['courtyard_low_Miss'];
+	$batter_high_Scored=(int)$_POST['batter_high_Scored'];
+	$batter_low_Scored=(int)$_POST['batter_low_Scored'];
+	$batter_high_Miss=(int)$_POST['batter_high_Miss'];
+	$batter_low_Miss=(int)$_POST['batter_low_Miss'];
+	$courtyard_high_Scored=(int)$_POST['courtyard_high_Scored'];
+	$courtyard_low_Scored=(int)$_POST['courtyard_low_Scored'];
+	$courtyard_high_Miss=(int)$_POST['courtyard_high_Miss'];
+	$courtyard_low_Miss=(int)$_POST['courtyard_low_Miss'];
 	//Climbing variables
-	if($_POST['challenge_Sucess']== "on"){$challenge_Sucess=1;}
-	if($_POST['Scaled_Sucess']== "on"){$Scaled_Sucess=1;}
+	if((int)$_POST['challenge_Sucess']== "on"){$challenge_Sucess=1;}
+	if((int)$_POST['Scaled_Sucess']== "on"){$Scaled_Sucess=1;}
 	//Defense Rating
 	if(($_POST['defense'] =='0')){$defense="0";}
 	elseif(($_POST['defense'] =='25')){$defense="<25";}
 	elseif(($_POST['defense'] =='50')){$defense="50";}
 	elseif(($_POST['defense'] =='75')){$defense=">75";}
 	//Robot Issues
-	if($_POST['no_show']== "on"){$no_show=1;}
-	if($_POST['mech_fail']== "on"){$mech_fail=1;}
-	if($_POST['lost_comm']== "on"){$lost_comms=1;}
-	if($_POST['stuck']== "on"){$stuck=1;}
-	if($_POST['tipped']== "on"){$tipped=1;}
-	$fouls=$_POST['fouls'];
-	$tech_fouls=$_POST['tech_fouls'];
+	if((int)$_POST['no_show']== "on"){$no_show=1;}
+	if((int)$_POST['mech_fail']== "on"){$mech_fail=1;}
+	if((int)$_POST['lost_comm']== "on"){$lost_comms=1;}
+	if((int)$_POST['stuck']== "on"){$stuck=1;}
+	if((int)$_POST['tipped']== "on"){$tipped=1;}
+	$fouls=(int)$_POST['fouls'];
+	$tech_fouls=(int)$_POST['tech_fouls'];
 	//Driver Data
-	$drive_manuverability=$_POST['drive_manuverability'];
-	$pushing=$_POST['pushing'];
-	$Defense_Pushing=$_POST['Defense_Pushing'];
-	$Ball_Control=$_POST['Ball_Control'];
+	$drive_manuverability=(int)$_POST['drive_manuverability'];
+	$pushing=(int)$_POST['pushing'];
+	$Defense_Pushing=(int)$_POST['Defense_Pushing'];
+	$Ball_Control=(int)$_POST['Ball_Control'];
 	//Comments
 	$notes=$_POST['notes'];
 	//$fields = array($matchNum,$teamNum,$def_type_1,$def_type_2,$def_type_3,$def_type_4,$def_type_5,$drive_man,$notes);
@@ -448,7 +447,7 @@ include("db_connect.php");
 	echo"Successfully added info";	
 	}
 	else {
-	echo mysqli_errno($mysqli);;	
+	printf("Errormessage: %s\n", $mysqli->error);
 	}
 	//}	
 	//}	
