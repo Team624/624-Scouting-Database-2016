d803f1ffb7678ffae1aa21a5d00aa6b44084dedf
Lenovo 2.0 <Willalasater@gmail.com>
2016-02-26 19:30:19 -0600

    clickly teams

--- MatchSchedule.php
+++ MatchSchedule.php
@@ -39,14 +39,14 @@ include("db_connect.php");
 	{
 ?>	
 		<tr class="scheduleZenbra">
-			<td class="side-bar"><b><?=$row["match_number"];?></b></td>
+			<td class="side-bar"><a href="MatchInfoDisplay.php?match=<?=$row["match_number"];?>" class="side-bar-a"><b><?=$row["match_number"];?></b></a></td>
 			<td class="time"><b><?=$row['time'];?></b></td>
-			<td class="<?=$row['red_1']=="624"?'green':'red'?>"><a href="TeamInfoDisplay.php?team=<?=$row['red_1'];?>"><?=$row['red_1'];?></a></td>
-			<td class="<?=$row['red_2']=="624"?'green':'red'?>"><?=$row['red_2'];?></td>
-			<td class="<?=$row['red_3']=="624"?'green':'red'?>"><?=$row['red_3'];?></td>
-			<td class="<?=$row['blue_1']=="624"?'green':'blue'?>"><?=$row['blue_1'];?></td>
-			<td class="<?=$row['blue_2']=="624"?'green':'blue'?>"><?=$row['blue_2']?></td>
-			<td class="<?=$row['blue_3']=="624"?'green':'blue'?>"><?=$row['blue_3']?></td>
+			<td class="<?=$row['red_1']=="624"?'green':'red'?>"><a href="TeamInfoDisplay.php?team=<?=$row['red_1'];?>" class="<?=$row['red_1']=="624"?'green-a':'red-a'?>"><?=$row['red_1'];?></a></td>
+			<td class="<?=$row['red_2']=="624"?'green':'red'?>"><a href="TeamInfoDisplay.php?team=<?=$row['red_2'];?>" class="<?=$row['red_2']=="624"?'green-a':'red-a'?>"><?=$row['red_2'];?></a></td>
+			<td class="<?=$row['red_3']=="624"?'green':'red'?>"><a href="TeamInfoDisplay.php?team=<?=$row['red_3'];?>" class="<?=$row['red_3']=="624"?'green-a':'red-a'?>"><?=$row['red_3'];?></a></td>
+			<td class="<?=$row['blue_1']=="624"?'green':'blue'?>"><a href="TeamInfoDisplay.php?team=<?=$row['blue_1'];?>" class="<?=$row['blue_1']=="624"?'green-a':'blue-a'?>"><?=$row['blue_1'];?></a></td>
+			<td class="<?=$row['blue_2']=="624"?'green':'blue'?>"><a href="TeamInfoDisplay.php?team=<?=$row['blue_2'];?>" class="<?=$row['blue_2']=="624"?'green-a':'blue-a'?>"><?=$row['blue_2'];?></a></td>
+			<td class="<?=$row['blue_3']=="624"?'green':'blue'?>"><a href="TeamInfoDisplay.php?team=<?=$row['blue_3'];?>" class="<?=$row['blue_3']=="624"?'green-a':'blue-a'?>"><?=$row['blue_3'];?></a></td>
 		</tr>
 <?php	
 	}
